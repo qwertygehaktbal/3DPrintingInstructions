b357c0271d6ad55d6010e6d3cd0ecb59a3c0ef6c
qwertygehaktbal <qwertygehaktbal@gmail.com>
2025-05-08 14:43:18 +0300

    changes to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Tutorial from "./pages/Tutorial";
 import Instructions from "./pages/Instructions";
 import Test from "./pages/Test";
 import End from "./pages/End";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 
 export const App = () => {
